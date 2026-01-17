25dfb5ac5d9b0bb197125a1db7e446636e45677b
Sawan kumar <rm7453460@gmail.com>
2026-01-17 13:19:16 +0530

    add login and logout logic

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -120,8 +120,50 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
 
-    
+
+    const loggedInuser = user.findById(user._id)
+    .select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInuser, accessToken, refreshToken
+            }, 
+            "User logged In Successfully"
+        )
+    )
 
 })
 
-export { registerUser, loginUser } 
+
+const logoutuser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,{
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {} , "User logged out"))
+})
+
+export { registerUser, loginUser, logoutuser } 
